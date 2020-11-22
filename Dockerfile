ca760bbc01e96d63e5700e17e28e878129a10804
Viktoriia Ivanova <iv.vik.19.07@gmail.com>
2020-11-22 19:43:36 +0200

    change dockerfile, config file and connection to db

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:11-jre
-WORKDIR D: /3 курс/ТРСПО/lab3/hike/out/artifacts/hike_jar
-EXPOSE 8088
+FROM openjdk:14.0.2-jdk
+COPY ./target/*.jar /app.jar
+EXPOSE 8087
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/hike/api/HikeController.java
+++ src/main/java/com/example/hike/api/HikeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/hike")
+@RequestMapping("/")
 public class HikeController {
 
     private final HikeService hikeService;
